f712bfc40d1118d26873bc76a581b4cc91ce8cbb
Benedikt <benedikt.imminger@stud.uni-regensburg.de>
2017-01-12 13:47:32 +0100

    Update: DB Helper

--- app/src/main/java/com/example/puppetmaster/vokabeltrainer/StartScreen.java
+++ app/src/main/java/com/example/puppetmaster/vokabeltrainer/StartScreen.java
@@ -1,7 +1,13 @@
 package com.example.puppetmaster.vokabeltrainer;
 
+import android.content.Context;
+import android.database.SQLException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.puppetmaster.vokabeltrainer.DatabaseCommunication.DataBaseHelper;
+
+import java.io.IOException;
 
 public class StartScreen extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class StartScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
+
+        DataBaseHelper myDbHelper = new DataBaseHelper();
+        myDbHelper = new DataBaseHelper(this);
+
+        try {
+
+            myDbHelper.createDataBase();
+
+        } catch (IOException ioe) {
+
+            throw new Error("Unable to create database");
+
+        }
+
+        try {
+
+            myDbHelper.openDataBase();
+
+        }catch(SQLException sqle){
+
+            throw sqle;
+
+        }
     }
 }
