bd7a4c6eca6741d02b57db434d0e720ba39a722a
Benedikt <benedikt.imminger@stud.uni-regensburg.de>
2017-02-09 13:35:41 +0100

    Updated: DB with logging table

--- app/src/main/java/com/example/puppetmaster/vokabeltrainer/DatabaseCommunication/MyDatabase.java
+++ app/src/main/java/com/example/puppetmaster/vokabeltrainer/DatabaseCommunication/MyDatabase.java
@@ -19,7 +19,7 @@ import java.util.Date;
 public class MyDatabase extends SQLiteAssetHelper {
 
     private static final String DATABASE_NAME = "vocabDB.db";
-    private static final int DATABASE_VERSION = 4;
+    private static final int DATABASE_VERSION = 5;
 
     public MyDatabase(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
