4dafb25e92a608fd57dc0cf69ded78ab2b0870d3
Benedikt <benedikt.imminger@stud.uni-regensburg.de>
2017-02-10 13:26:49 +0100

    Updated: Custom Toolbar (Settings Icon)

--- app/src/main/java/com/example/puppetmaster/vokabeltrainer/StartScreen.java
+++ app/src/main/java/com/example/puppetmaster/vokabeltrainer/StartScreen.java
@@ -1,9 +1,13 @@
 package com.example.puppetmaster.vokabeltrainer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.puppetmaster.vokabeltrainer.Fragments.GameFragment;
@@ -12,6 +16,7 @@ import com.example.puppetmaster.vokabeltrainer.Fragments.ProfileFragment;
 import com.example.puppetmaster.vokabeltrainer.Fragments.TopicsFragment;
 
 public class StartScreen extends AppCompatActivity {
+    private Toolbar toolbar;
 
 
     @Override
@@ -19,15 +24,16 @@ public class StartScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_screen);
 
+        initBottomNavigation();
+        initToolBar();
 
         getFragmentManager().beginTransaction()
-                .replace(R.id.container, new HomeFragment())
+                .replace(R.id.container_start, new HomeFragment())
                 .commit();
 
-        setUpBottomNavigation();
     }
 
-    private void setUpBottomNavigation() {
+    private void initBottomNavigation() {
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(
@@ -37,27 +43,52 @@ public class StartScreen extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.action_start:
                                 getFragmentManager().beginTransaction()
-                                        .replace(R.id.container, new HomeFragment())
+                                        .replace(R.id.container_start, new HomeFragment())
                                         .commit();
                                 break;
                             case R.id.action_topics:
                                 getFragmentManager().beginTransaction()
-                                        .replace(R.id.container, new TopicsFragment())
+                                        .replace(R.id.container_start, new TopicsFragment())
                                         .commit();
                                 break;
                             case R.id.action_game:
                                 getFragmentManager().beginTransaction()
-                                        .replace(R.id.container, new GameFragment())
+                                        .replace(R.id.container_start, new GameFragment())
                                         .commit();
                                 break;
                             case R.id.action_profile:
                                 getFragmentManager().beginTransaction()
-                                        .replace(R.id.container, new ProfileFragment())
+                                        .replace(R.id.container_start, new ProfileFragment())
                                         .commit();
                                 break;
+                            default:
                         }
                         return true;
                     }
                 });
     }
+
+    public void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar_start);
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_settings:
+                Log.d("debug","activity: action home has clicked");
+                startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
