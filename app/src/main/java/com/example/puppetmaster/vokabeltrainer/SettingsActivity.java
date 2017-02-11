1fbace05699389ff16d68f4a85cdb4c3589df6c5
Benedikt <benedikt.imminger@stud.uni-regensburg.de>
2017-02-11 21:58:34 +0100

    Updated: SettingsActivity functionality

--- app/src/main/java/com/example/puppetmaster/vokabeltrainer/SettingsActivity.java
+++ app/src/main/java/com/example/puppetmaster/vokabeltrainer/SettingsActivity.java
@@ -6,29 +6,60 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.example.puppetmaster.vokabeltrainer.DatabaseCommunication.MyDatabase;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
+    private MyDatabase db;
     private Spinner spinnerStart;
+    private String strStart;
     private Spinner spinnerEnd;
+    private String strEnd;
+    private EditText etWorkload;
+    private int workload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        db = new MyDatabase(this);
+        loadSettings();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                saveSettings();
+                Snackbar.make(view, "Settings have been saved", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
 
+
+    }
+
+    private void loadSettings() {
+        // TODO Vermutlich ist es schöner wenn man die Uhrzeiten als Int speichert
+        ArrayList<Object> settings = db.getSettings();
+        workload = (int) settings.get(0);
+        strStart = settings.get(1).toString();
+        strEnd = settings.get(2).toString();
+        initSpinner();
+        initWorkload();
+    }
+
+    private void initWorkload() {
+        etWorkload = (EditText) findViewById(R.id.et_workload);
+        etWorkload.setText(workload + "");
+    }
+
+    private void initSpinner() {
         spinnerStart = (Spinner) findViewById(R.id.spinner_start);
         spinnerEnd = (Spinner) findViewById(R.id.spinner_end);
         ArrayAdapter<String> adapter;
@@ -51,5 +82,26 @@ public class SettingsActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerStart.setAdapter(adapter);
         spinnerEnd.setAdapter(adapter);
+
+        setSpinnerValue(spinnerStart, strStart);
+        setSpinnerValue(spinnerEnd, strEnd);
+    }
+
+    private void saveSettings() {
+        workload = Integer.parseInt(etWorkload.getText().toString());
+        strStart = spinnerStart.getSelectedItem().toString();
+        strEnd = spinnerEnd.getSelectedItem().toString();
+        db.saveSettings(workload, strStart, strEnd);
+        }
+
+    private void setSpinnerValue(Spinner spinner, String myString)
+    {
+        int index = 0;
+        for(int i = 0; i < spinner.getCount(); i++){
+            if(spinner.getItemAtPosition(i).toString().equals(myString)){
+                spinner.setSelection(i);
+                break;
+            }
+        }
     }
 }
