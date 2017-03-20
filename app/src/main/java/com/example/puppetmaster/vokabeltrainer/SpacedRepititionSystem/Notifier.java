33a89429faa543afcc19d0e051229cc7f5fec4c6
fuchsflo90 <ff6879ff>
2017-03-20 18:04:24 +0100

    push sachen

--- app/src/main/java/com/example/puppetmaster/vokabeltrainer/SpacedRepititionSystem/Notifier.java
+++ app/src/main/java/com/example/puppetmaster/vokabeltrainer/SpacedRepititionSystem/Notifier.java
@@ -1,13 +1,54 @@
 package com.example.puppetmaster.vokabeltrainer.SpacedRepititionSystem;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import java.util.Calendar;
+
 /**
  * Created by florian on 12.01.17.
  */
 
 public class Notifier {
 
-    public void Notifier(){
-        //TODO
+    private Intent alarmIntent;
+    private PendingIntent pendingIntent;
+    private AlarmManager manager;
+    private SharedPreferences prefs;
+
+    public void Notifier(Context context){
+
+        this.prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        if (!prefs.getBoolean("firstTime", false)){
+
+            this.alarmIntent = new Intent(context, AlarmReceiver.class);
+            this.pendingIntent = PendingIntent.getBroadcast(context, 0, alarmIntent, 0);
+
+            this.manager = (AlarmManager) context.getSystemService(context.ALARM_SERVICE);
+
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTimeInMillis(System.currentTimeMillis());
+
+            //TODO zeit richtig setzen
+            calendar.set(Calendar.HOUR_OF_DAY, 7);
+            calendar.set(Calendar.MINUTE, 0);
+            calendar.set(Calendar.SECOND, 1);
+
+
+            //TODO für jeden zeitblock einen alarm setzen
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("firstTime", true);
+            editor.apply();
+        }
+
     }
 
 }
